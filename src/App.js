ef24ea3d23309c44f00fb1d57841cb9c4326ebde
TMats <takomats@hotmail.com>
2021-06-25 16:15:09 -0400

    added App and indexfile.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
-import React, { useReducer, useRef } from "react";
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
+import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
 
-const TodoList = () => {
-  // Placeholder array
-  const items = [];
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
-  return (
-    <div className="container text-center">
-      <h1>Create a Todo List!</h1>
-      <form className="form-group mt-5" onSubmit={handleSubmit}>
-        <input className="form-control" placeholder="Start typing what you need to do..." />
-        <button className="btn btn-success mt-3 mb-5" type="submit">
-          Add to List
-        </button>
-      </form>
-      <h4>My Todo List:</h4>
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li className="list-group-item" key={item.id}>
-            {item.name}{" "}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <Wrapper />
+            <Footer />
+        </div>
+    );
 };
 
-export default TodoList;
+export default App;
