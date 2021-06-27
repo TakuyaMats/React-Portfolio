52c2f0244ff4ebf71c5c56692254833fa2db4ae3
TMats <takomats@hotmail.com>
2021-06-26 20:36:34 -0400

    got my router setup for my pages.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import Wrapper from "./components/Wrapper";
-import Footer from "./components/Footer";
+import NavMenu from "./components/Navbar/index"
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
-        <div>
-            <Navbar />
-            <Wrapper />
-            <Footer />
-        </div>
+        <Router>
+            <NavMenu />
+            <Switch>
+                <Route path="/about">
+                    <About />
+                </Route>
+                <Route path="/projects">
+                    <Projects />
+                </Route>
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+            </Switch>
+
+            <Home />
+        </Router>
     );
 };
 
